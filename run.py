88c3d74c05faf07b212c64b667644e25c15226f2
Gaurav Jagpal <gauravjagpal@hotmail.com>
2024-04-14 12:38:46 +0100

    add main function to call all functions. correct validation statement to only return one error message

--- run.py
+++ run.py
@@ -15,4 +15,37 @@ SHEET = GSPREAD_CLIENT.open('subshop')
 items = SHEET.worksheet('items')
 data = items.get_all_values()
 
-print(data)
+def input_type():
+    """
+    User input for whether to add a new item or to update an item
+    """
+    while True:
+        print('Would you like to add a new item or update sales figures?')
+        data_str = input('Please enter "new" for new item or "update" for updating an item: \n')
+
+        if validate_data(data_str):
+            break
+
+    return data_str
+
+def validate_data(input):
+    """
+    Test user input matches options provided
+    """
+    try:
+        if (input not in ("new", "update")):
+            raise ValueError(f'You entered {input}, you need to enter "new" or "update" to continue')
+    except ValueError as e:
+        print(f'Invalid data: {e}, please try again. \n')
+
+    return True
+
+
+def main():
+    """
+    run all functions
+    """
+    input_type()
+
+print("Welcome to the Subshop")
+main()
