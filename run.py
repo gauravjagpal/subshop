d2ca4d58865049d9002a113a79584de411f8d6b4
Gaurav Jagpal <gauravjagpal@hotmail.com>
2024-04-15 10:29:51 +0100

    update stock tab as well if new item is added. Error thrown out if item is new and already in list

--- run.py
+++ run.py
@@ -14,7 +14,9 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('subshop')
 
 items = SHEET.worksheet('items')
-data = items.get_all_values()
+items_data = items.get_all_values()
+stock = SHEET.worksheet('stock')
+stock_data = items.get_all_values()
 
 def input_type():
     """
@@ -48,34 +50,38 @@ def update(data):
         Add new items into item tab
         """
         item_name = input('Please enter product name: \n')
-        item_code = item_name[0:3] + str(random.randrange(100,999))
-        min_stock = int(input('Please enter the minimum stock required: \n'))
-        bake_time_mins = input('Please enter how long the item takes to cook (in minutes): \n')
-        cost = float(input('How much does this item cost to prepare? (e.g 0.20) \n'))
-        sale_price = float(input('How much will this item sell for: (e.g 1.30)\n'))
-        profit = sale_price - cost
-        items_to_update = [item_name, item_code, min_stock, bake_time_mins, cost, sale_price, profit]
-        items.append_row(items_to_update)
-        return items_to_update
+        df = SHEET.worksheet('items')
+        first_col = df.col_values(1)
+        if item_name in first_col:
+            print('This item is already being produced. Please choose to update instead')
+        else:
+            item_code = item_name[0:3] + str(random.randrange(100,999))
+            min_stock = int(input('Please enter the minimum stock required: \n'))
+            bake_time_mins = int(input('Please enter how long the item takes to cook (in minutes): \n'))
+            cost = float(input('How much does this item cost to prepare? (e.g 0.20) \n'))
+            sale_price = float(input('How much will this item sell for: (e.g 1.30)\n'))
+            profit = sale_price - cost
+            items_to_update = [item_name, item_code, min_stock, bake_time_mins, cost, sale_price, profit]
+            items.append_row(items_to_update)
+            sales = 0
+            stock_on_hand = min_stock - sales
+            to_bake = min_stock - stock_on_hand
+            stock_to_update = [item_name, item_code, sales, min_stock, stock_on_hand, to_bake]
+            stock.append_row(stock_to_update)
+            return items_to_update
         
     elif data == "update":
         """
         Update existing items
         """
         item_name = input('Please enter product name: \n')
-        df = SHEET.worksheet('items')
-        first_col = df.col_values(1)
-        for i in first_col:
-            if i == item_name:
-                print('This item is already being produced. Please choose to update instead')
-            else:
-                #item_code = *VLOOKUP*
-                sales = int(input('Please enter how many items were sold: \n'))
-                min_stock = int(input('Please enter the minimum stock required: \n'))
-                stock_on_hand = min_stock - sales
-                to_bake = min_stock - stock_on_hand
-                items_to_update = [item_name, item_code, sales, min_stock, stock_on_hand, to_bake]
-        return data
+        #item_code = *VLOOKUP*
+        sales = int(input('Please enter how many items were sold: \n'))
+        min_stock = int(input('Please enter the minimum stock required: \n'))
+        stock_on_hand = min_stock - sales
+        to_bake = min_stock - stock_on_hand
+        stock_to_update = [item_name, item_code, sales, min_stock, stock_on_hand, to_bake]
+    return data
 
 
 
