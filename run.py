1e7281360422cd16559649c3e96c2577299aa426
Gaurav Jagpal <gauravjagpal@hotmail.com>
2024-04-18 17:59:51 +0100

    add delete function for users to remove a row

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from google.oauth2.service_account import Credentials
 import random
 import pandas as pd
 import numpy as np
-
+import openpyxl
 
 # Set scope for Google IAM authentication for the APIs the program has
 # access to.
@@ -43,11 +43,11 @@ is needed for the next day.
 The Subshop tool will ask you to provide information such as:
     - new/update (to know whether you plan on adding a new item or update an existing one)
     - If you select "new", it will also ask you for the item name, the min stock required,
-      the time to bake, cost of the item, sale price and it will calculate the profit.
+      the time to make, cost of the item, sale price and it will calculate the profit.
       It will then append this to the "items" tab and "stock" tab of your worksheet.
     - If you select "update", it will automatically bring the item_code from the "items" tab,
       it will ask you how many items were sold, the minimum stock required and then it will
-      calculate the stock on hand and the amount you need to bake.
+      calculate the stock on hand and the amount you need to make.
 
 <==============================================================================================>
     ''')
@@ -92,16 +92,17 @@ def update(data):
         else:
             item_code = item_name[0:3] + str(random.randrange(100,999))
             min_stock = int(input('Please enter the minimum stock required: \n'))
-            bake_time_mins = int(input('Please enter how long the item takes to cook (in minutes): \n'))
+            make_time_mins = int(input('Please enter how long the item takes to cook (in minutes): \n'))
             cost = float(input('How much does this item cost to prepare? (e.g 0.20) \n'))
             sale_price = float(input('How much will this item sell for: (e.g 1.30)\n'))
             profit = sale_price - cost
-            items_to_update = [item_name, item_code, min_stock, bake_time_mins, cost, sale_price, profit]
+            items_to_update = [item_name, item_code, min_stock, make_time_mins, cost, sale_price, profit]
             items.append_row(items_to_update)
             sales = 0
             stock_on_hand = min_stock - sales
-            to_bake = min_stock - stock_on_hand
-            stock_to_update = [item_name, item_code, sales, min_stock, stock_on_hand, to_bake]
+            to_make = min_stock - stock_on_hand
+            total_profit = profit * sales
+            stock_to_update = [item_name, item_code, sales, min_stock, stock_on_hand, to_make, total_profit]
             stock.append_row(stock_to_update)
         
     elif data == "update":
@@ -121,20 +122,21 @@ def update(data):
             #User inputs
             sales = int(input('Please enter how many items were sold:\n'))
             min_stock = int(input('Please enter the minimum stock required for tomorrow:\n'))
-            bake_time_mins = int(input('How many minutes will it take to bake the item:\n'))
+            make_time_mins = int(input('How many minutes will it take to make the item:\n'))
+            cost = float(input('How much does this item cost to prepare? (e.g 0.20) \n'))
+            sale_price = float(input('How much will this item sell for: (e.g 1.30)\n'))
             #Formulas
             stock_on_hand = min_stock - sales
-            to_bake = min_stock - stock_on_hand
-            
-            
-            
-            
-            stock.update_cell(index+1,3, sales)
-            stock.update_cell(index+1,4, min_stock)
-            stock.update_cell(index+1,5, stock_on_hand)
-            stock.update_cell(index+1,6, to_bake)
+            to_make = min_stock - stock_on_hand
+            profit = sale_price - cost
+            total_profit = profit * sales
 
-
+            #Update items tab
+            items.update_cell(index+1,3, min_stock)
+            items.update_cell(index+1,4, make_time_mins)
+            items.update_cell(index+1,5, cost)
+            items.update_cell(index+1,6, sale_price)
+            items.update_cell(index+1,7, profit)
 
             #update stock page
             df1 = pd.DataFrame(items_data)
@@ -144,35 +146,31 @@ def update(data):
             stock.update_cell(index1+1,3, sales)
             stock.update_cell(index1+1,4, min_stock)
             stock.update_cell(index1+1,5, stock_on_hand)
-            stock.update_cell(index1+1,6, to_bake)
+            stock.update_cell(index1+1,6, to_make)
+            stock.update_cell(index1+1,7, total_profit)
         else:
             print('This item is not in the current stock. Please add as a new item.\n')
         return data
     
-#    elif data == "delete":
-#        item_name = input('Please enter the product name of what you would like to remove from your store: \n')
-#        df = SHEET.worksheet('items')
-#        first_col_items = df.col_values(1)
-#        if item_name in first_col_items:
-#            df1 = pd.DataFrame(items_data)
-#            df2 = pd.DataFrame(stock_data)
-#            grade = np.where(df1[:][0] == item_name)
-#            index = (grade[0])[0]
-#            items.delete_rows(index)
-#            stock.delete_rows(index)
-#            items2 = SHEET.worksheet('items')
-#            items_data2 = items2.get_all_values()
-#            stock2 = SHEET.worksheet('stock')
-#            stock_data2 = stock2.get_all_values()
-        
- #       else:
-  #          print('You do not currently produce this item')
+    elif data == "delete":
+        item_name = input('Please enter the product name of what you would like to remove from your store: \n')
+        df = SHEET.worksheet('items')
+        first_col_items = df.col_values(1)
+        if item_name in first_col_items:
+            df1 = pd.DataFrame(items_data)
+            #df2 = pd.DataFrame(stock_data)
+            grade = np.where(df1[:][0] == item_name)
+            index = int((grade[0])[0])
+            SHEET.worksheet('items').delete_rows(index+1)
+            SHEET.worksheet('stock').delete_rows(index+1)
+    else:
+        print('You do not currently produce this item')
 
 
-#def reuse(data):
+#def reuse():
 #    again = input('Would you like to input more data? Please enter "Yes" or any key to escape\n')
 #    if again == 'yes':
-#        update(data)
+#        main()
 
 
 def return_data(stock_data):
@@ -193,12 +191,14 @@ def main():
     items_data2 = items2.get_all_values()
     stock2 = SHEET.worksheet('stock')
     stock_data2 = stock2.get_all_values()
-    #print('\n')
-    #print('Your store has the below requirements:')
-    #return_data(items_data2)
-    #print('\n')
-    #print('You stock is as below')
+    print('\n')
+    print('Your stores product details are as below:')
+    return_data(items_data2)
+    print('\n')
+    print('You store has the below requirements')
     return_data(stock_data2)
-#    reuse()
+
+
 print("Welcome to the Subshop \n")
 main()
+#reuse()
