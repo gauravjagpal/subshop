78558b5ef6908c3c6d467e86a3c69aec0c0b6e13
Gaurav Jagpal <gauravjagpal@hotmail.com>
2024-04-18 18:26:44 +0100

    give user more options for initial input, new, update, delete

--- run.py
+++ run.py
@@ -65,13 +65,19 @@ def input_type():
 
     return data_str
 
+# Giving the user more variety for initial input
+new = ["new", "NEW", "New"]
+updater = ["update", "UPDATE", "Update"]
+delete = ["delete", "DELETE", "Delete"]
+# Combining the options
+user_options = new + updater + delete
 
 def validate_data(data):
     """
     Test user input matches options provided
     """
     try:
-        if (data not in ("new", "update", "delete")):
+        if data not in user_options:
             raise ValueError(f'You entered {input}, you need to enter "new", "update" or "delete" to continue')
     except ValueError as e:
         print(f'Invalid data: {e}, please try again. \n')
@@ -80,7 +86,7 @@ def validate_data(data):
 
 
 def update(data):
-    if data == "new":
+    if data in new:
         """
         Add new items into item tab
         """
@@ -105,7 +111,7 @@ def update(data):
             stock_to_update = [item_name, item_code, sales, min_stock, stock_on_hand, to_make, total_profit]
             stock.append_row(stock_to_update)
         
-    elif data == "update":
+    elif data in updater:
         """
         Update existing items located in stock tab
         """
@@ -118,13 +124,14 @@ def update(data):
             df = pd.DataFrame(items_data)
             grade = np.where(df[:][0] == item_name)
             index = (grade[0])[0]
-            #item_code = (items_data[index])[1]
+
             #User inputs
             sales = int(input('Please enter how many items were sold:\n'))
             min_stock = int(input('Please enter the minimum stock required for tomorrow:\n'))
             make_time_mins = int(input('How many minutes will it take to make the item:\n'))
             cost = float(input('How much does this item cost to prepare? (e.g 0.20) \n'))
             sale_price = float(input('How much will this item sell for: (e.g 1.30)\n'))
+
             #Formulas
             stock_on_hand = min_stock - sales
             to_make = min_stock - stock_on_hand
@@ -139,10 +146,9 @@ def update(data):
             items.update_cell(index+1,7, profit)
 
             #update stock page
-            df1 = pd.DataFrame(items_data)
-            grade1 = np.where(df1[:][0] == item_name)
+            df = pd.DataFrame(items_data)
+            grade1 = np.where(df[:][0] == item_name)
             index1 = (grade1[0])[0]
-            #item_code = (items_data[index1])[1]
             stock.update_cell(index1+1,3, sales)
             stock.update_cell(index1+1,4, min_stock)
             stock.update_cell(index1+1,5, stock_on_hand)
@@ -152,25 +158,26 @@ def update(data):
             print('This item is not in the current stock. Please add as a new item.\n')
         return data
     
-    elif data == "delete":
+    elif data in delete:
         item_name = input('Please enter the product name of what you would like to remove from your store: \n')
         df = SHEET.worksheet('items')
         first_col_items = df.col_values(1)
         if item_name in first_col_items:
-            df1 = pd.DataFrame(items_data)
-            #df2 = pd.DataFrame(stock_data)
-            grade = np.where(df1[:][0] == item_name)
+            df = pd.DataFrame(items_data)
+            grade = np.where(df[:][0] == item_name)
             index = int((grade[0])[0])
             SHEET.worksheet('items').delete_rows(index+1)
             SHEET.worksheet('stock').delete_rows(index+1)
+        else:
+            print('You do not currently produce this item')    
     else:
         print('You do not currently produce this item')
 
 
 def reuse():
     again = input('Would you like to input more data? Please enter "Yes" or any key to escape\n')
-    yes = {'yes','Yes', 'y', 'Y'}
-    if again == 'Yes' :
+    yes = ['yes','Yes', 'y', 'Y']
+    if again in yes :
         main()
 
 
@@ -197,9 +204,10 @@ def main():
     print('\n')
     print('You store has the below requirements')
     return_data(stock_data2)
+    reuse()
 
 #Run intro first as if the user wants to reuse the tool, we don't want the intro to run again
 run_intro()
 print("Welcome to the Subshop \n")
 main()
-reuse()
+
